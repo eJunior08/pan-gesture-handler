271e0400d49b501991c2062125d1e566578c65bf
Elson Oliveira <elsonfojr@gmail.com>
2021-08-11 12:08:40 -0300

    Pan gesture works

--- App.tsx
+++ App.tsx
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Dimensions, View, StyleSheet, StatusBar } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+} from "react-native-reanimated";
+import { clamp } from "react-native-redash";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Card, Cards, CARD_HEIGHT, CARD_WIDTH } from "./src/components";
+
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+/* interface GestureProps {
+  width: number;
+  height: number;
+} */
+
+const App = (/* { width, height }: GestureProps */) => {
+  const boundX = width - CARD_WIDTH;
+  const boundY = height - CARD_HEIGHT;
+
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      ctx.offsetX = translateX.value;
+      ctx.offsetY = translateY.value;
+    },
+    onActive: (event, ctx) => {
+      translateX.value = clamp(ctx.offsetX + event.translationX, 0, boundX);
+      translateY.value = clamp(ctx.offsetY + event.translationY, 0, boundY);
+    },
+    onEnd: (event) => {
+      translateX.value = withDecay({
+        velocity: event.velocityX,
+        clamp: [0, boundX],
+      });
+      translateY.value = withDecay({
+        velocity: event.velocityY,
+        clamp: [0, boundY],
+      });
+    },
+  });
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
+  console.log({ width, height });
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <PanGestureHandler {...{ onGestureEvent }}>
+        <Animated.View {...{ style }}>
+          <Card card={Cards.Card1} />
+        </Animated.View>
+      </PanGestureHandler>
+    </View>
+  );
+};
+
+export default App;
